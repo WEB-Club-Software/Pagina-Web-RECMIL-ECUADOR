74d54fb947b4298c3fef40eb789799142bbe0a1e
Antoni30 <Antoni30@github.com>
2023-07-10 00:29:43 -0500

    atomo Descriptive Icon update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,6 @@
-import { CSSProperties } from "react";
-import DescriptiveIcon from "../components/atoms/DescriptiveIcon";
-import { faFacebook } from '@fortawesome/free-brands-svg-icons';
-import { faWhatsapp } from "@fortawesome/free-brands-svg-icons";
-
 export default function Page() {
-  const cssFig:CSSProperties={
-    color: 'rgb(64,96,150)',
-    width: "50px"
-  }
   return <h1>
     Hello, Next.js!
-    <DescriptiveIcon text="Facebook" icon={faFacebook} style={cssFig}/>
+   
     </h1>
 }
