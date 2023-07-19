a6521388cfc888b7acf5d40e0d073310dac3c434
Oswaldo <oswaldoj.tipan@gmail.com>
2023-07-18 21:44:19 -0500

    GoBackButtonCreated

--- src/components/atoms/GoBackButton.tsx
+++ src/components/atoms/GoBackButton.tsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { HTMLAttributeAnchorTarget, HTMLAttributes } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; 
+import { faArrowUp } from '@fortawesome/free-solid-svg-icons';
 
-const GoBackButton = () => {
+interface Props extends HTMLAttributes<HTMLButtonElement>{
+    isHome?:Boolean;
+}
+
+const GoBackButton = ({isHome}: Props) => {
     return (
-        <button>      
+        <button className={`flex gap-4 content-center items-center px-3 py-1 ${isHome} && 'invisible'`}>      
             <span className="text-small text-blue-400 font-semibold">Back</span>
-            <img src="/icons/arrow.svg" alt="back" className='rotate-180' />
+            <FontAwesomeIcon className='w-3 text-blue-400' icon={faArrowUp}/>
         </button> 
     );
 };
