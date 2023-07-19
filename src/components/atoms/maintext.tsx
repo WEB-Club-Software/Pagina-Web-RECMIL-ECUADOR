4187e10209a5b717267389dd407b4c2b505e4ca5
Tenkenoz <epobando1@espe.edu.ec>
2023-07-18 23:49:45 -0500

    Main text

--- src/components/atoms/maintext.tsx
+++ src/components/atoms/maintext.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import React from 'react';
 
-export default function maintext(main: string) {
-  return (
-    <h2>{main}</h2>
-  )
+interface Props {
+  main: string;
 }
+
+const MainText: React.FC<Props> = ({ main }) => {
+  return (
+    <h1 className="font-sans font-semibold">{main}</h1>
+  );
+};
+
+export default MainText;
 
 
 
