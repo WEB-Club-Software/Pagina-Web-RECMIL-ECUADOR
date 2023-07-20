6b2ef81f84ce15088932a46513e5e4d972ead385
Antoni <49771595+Antoni30@users.noreply.github.com>
2023-07-20 01:57:01 -0500

    Pwre 22 crear atomo acordeaon preguntas frecuentes (#8)
    * init accordin
    
    * atomo y molecula Acordeon
    
    ---------
    
    Co-authored-by: Antoni30 <Antoni30@github.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,7 @@
-import NextToButton from "@/components/atoms/NextToButton"
-import { faFacebook } from "@fortawesome/free-brands-svg-icons"
-import { faLandmark } from "@fortawesome/free-solid-svg-icons"
 
 export default function Page() {
-  
-  const style={
-    color:'red',
-    width:'50px'
-  }
+  return <h1>
+    Hello, Next.js!
+  </h1>
 
-  const opc: string[]=['home','about us']
-  return <NextToButton text={"Conocenos más"}/>
 }
