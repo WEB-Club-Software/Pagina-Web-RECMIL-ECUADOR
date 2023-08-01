9b002f0c81c78138e4a8123d42558a869d32bc78
Antoni30 <Antoni30@github.com>
2023-08-01 14:03:12 -0500

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
-import Contact from "@/components/molecules/Contact"
-import { faFacebook } from "@fortawesome/free-brands-svg-icons"
-import { faLandmark } from "@fortawesome/free-solid-svg-icons"
-
 
 export default function Page() {
 
-  const opc: string[]=['home','about us']
-  return <Contact /> 
+  return
+  <h1>
+    App
+  </h1>
 }
