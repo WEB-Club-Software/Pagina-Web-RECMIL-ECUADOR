53b7a09de0d279cd6b8c3ddab0b457f24fdcd788
Antoni30 <Antoni30@github.com>
2023-08-10 09:49:55 -0500

    reponsive web

--- src/components/atoms/AccordionItem.tsx
+++ src/components/atoms/AccordionItem.tsx
@@ -16,7 +16,7 @@ const AccordionItem: FC<AccordionItemProps> = ({ item, active, handleTogle }) =>
     const contentEl = useRef<HTMLDivElement | null>(null);
 
     return (
-        <li className={`bg-zinc-200 list-none p-6  border-solid border-black  ${active === item.id ? "rounded-lg" : "rounded-full"}`}>
+        <li className={`bg-zinc-200 list-none p-6  border-solid border-black   rounded-lg ${active === item.id ? "" : "rounded-full"}`}>
             <div>
                 <h2 onClick={() => handleTogle(item.id)} className='xs:text-md md:text-xl font-medium text-black flex justify-between cursor-pointer select-none'>
                     {item.title}<span className='text-gray-500'>{item.id === active ? "-" : "+"}</span>
